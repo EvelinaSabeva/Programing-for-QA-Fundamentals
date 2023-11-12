4ae2a74467fc0a8677cd3d7e7432bdbb9da83d1b
EvelinaSabeva <evelina_sabeva@abv.bg>
2023-11-13 00:13:31 +0200

    feat: Conditional Statetements Advanced

---
+++ ConditionalStatementsAdvancedExercise/05.Journey/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _05.Journey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            string destination = " ";
+            string type = " ";
+            double spendMoney = 0;
+
+            if (budget <= 100)
+            {
+                destination = "Bulgaria";
+
+                if (season == "summer")
+                {
+                    spendMoney = 0.3 * budget;
+                    type = "Camp";
+                }
+                else if (season == "winter")
+                {
+                    spendMoney = 0.7 * budget;
+                    type = "Hotel";
+                }
+            }
+            else if (budget <= 1000)
+            {
+                destination = "Balkans";
+
+                if (season == "summer")
+                {
+                    spendMoney = 0.4 * budget;
+                    type = "Camp";
+                }
+                else if (season == "winter")
+                {
+                    spendMoney = 0.8 * budget;
+                    type = "Hotel";
+                }
+            }
+            else if (budget > 1000)
+            {
+                destination = "Europe";
+                spendMoney = 0.9 * budget;
+                type = "Hotel";
+
+            }
+
+            Console.WriteLine($"Somewhere in {destination}");
+            Console.WriteLine($"{ type} - { spendMoney :F2}");
+
+        }
+    }
+}
---
+++ ConditionalStatementsAdvancedExercise/07.HotelRoom/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace _07.HotelRoom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            int countOfEvening = int.Parse(Console.ReadLine());
+            double priceForStudio = 0.0;
+            double priceForApartament = 0.0;
+
+
+
+            //За студио, при повече от 7 нощувки през май и октомври: 5 % намаление.
+            //За студио, при повече от 14 нощувки през май и октомври: 30 % намаление.
+            //За студио, при повече от 14 нощувки през юни и септември: 20 % намаление.
+            //За апартамент, при повече от 14 нощувки, без значение от месеца : 10 % намаление
+
+            double fivePercent = 5.0 / 100.0;
+            double tenPercent = 10.0 / 100.0;
+            double twentyPercent = 20.0 / 100.0;
+            double thirtyPercent = 30.0 / 100.0;
+
+            if (month == "May" || month == "October")
+            {
+                priceForStudio = 50.0;
+                priceForApartament = 65.0;
+
+                if (countOfEvening > 7 && countOfEvening <= 14)
+                {
+                    priceForStudio -= priceForStudio * fivePercent;
+
+                }
+                else if (countOfEvening > 14)
+                {
+                    priceForStudio -= priceForStudio * thirtyPercent;
+                    priceForApartament -= priceForApartament * tenPercent;
+                }
+
+            }
+            else if (month == "June" || month == "September")
+            {
+                priceForStudio = 75.20;
+                priceForApartament = 68.70;
+
+                if (countOfEvening > 14)
+                {
+                    priceForStudio -= priceForStudio * twentyPercent;
+                    priceForApartament -= priceForApartament * tenPercent;
+
+                }
+            }
+            else if (month == "July" || month == "August")
+            {
+                priceForStudio = 76.0;
+                priceForApartament = 77.0;
+
+                if (countOfEvening > 14)
+                {
+                    priceForApartament -= priceForApartament * tenPercent;
+                }
+            }
+            //else
+            //{
+            //    if (countOfEvening > 14)
+            //    {
+            //        priceForApartament -= priceForApartament * 0.1;
+            //    }
+            //}
+
+
+            double totalPriceForApartament = countOfEvening * priceForApartament;
+            double totalPriceForStudio = countOfEvening * priceForStudio;
+            Console.WriteLine($"Apartment: { totalPriceForApartament:F2} lv.");
+            Console.WriteLine($"Studio: {totalPriceForStudio:F2} lv.");
+        }
+    }
+}
